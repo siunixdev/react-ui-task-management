8bb3e2d5617ba4c1e76398c8effee61ff9cee186
siunix <abd.siunix@gmail.com>
2022-03-11 22:12:13 +0700

    initial color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#09C7E1',
+        secondary: '#A338D6',
+        dark: '#323338',
+        success: '#00CB72',
+        warning: '#FFB132',
+        danger: '#ED4141',
+      },
+    },
   },
   plugins: [],
 }
