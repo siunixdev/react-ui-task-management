685c29cd13c7ca96f02f3b70809a0a999f6f6e10
siunix <abd.siunix@gmail.com>
2022-03-12 01:23:21 +0700

    Add sidebar component

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import './Sidebar.sass'
 
 function Sidebar() {
   return (
-    <div className='w-72 p-4 h-full border-r sticky top-0 bg-white z-50'>
-      sidebar
+    <div className='sidebar'>
+      <div className='mb-6'>
+        <span className='text-lg font-bold'>
+          ICON
+        </span>
+      </div>
+      <div>
+        <ul className='space-y-2'>
+          <li
+            className='flex items-center text-left w-full text-gray-500 hover:bg-primary hover:text-white rounded-lg p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg>
+            <span className='ml-2'>Home</span>
+          </li>
+          <li
+            className='flex items-center text-left w-full text-gray-500 hover:bg-primary hover:text-white rounded-lg p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+            </svg>
+            <span className='ml-2'>My Task</span>
+          </li>
+          <li
+            className='flex items-center text-left w-full text-gray-500 hover:bg-primary hover:text-white rounded-lg p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <span className='ml-2'>My Plan</span>
+          </li>
+          <li
+            className='flex items-center text-left w-full text-gray-500 hover:bg-primary hover:text-white rounded-lg p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+            </svg>
+            <span className='ml-2'>Profile</span>
+          </li>
+          <li
+            className='flex items-center text-left w-full text-gray-500 hover:bg-primary hover:text-white rounded-lg p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span className='ml-2'>Reporting</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
